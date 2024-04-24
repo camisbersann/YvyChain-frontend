12d8a48aaf6ba35acdf66ed398d42f610ba8ac3a
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-24 11:22:23 -0300

    criando colunas de texto

--- src/screens/AboutUs/index.jsx
+++ src/screens/AboutUs/index.jsx
@@ -1,11 +1,21 @@
-import { View, Text,  } from 'react-native';
+import { View, Text, ImageBackground, ScrollView } from 'react-native';
 import styles from './styles';
 import Title from '../../components/Title';
 
 export default function AboutUs() {
   return (
+    <ImageBackground source={require("../../../assets/fundoaboutus2.jpg")} style={styles.container}>
     <View style={styles.container}>
       <Title title={"CONHEÇA A NOSSA EMPRESA"}/>
+    <ScrollView>
+        <View style={styles.coluna}>
+            <View style={styles.coluna1}>
+            <Text style={styles.titulocoluna}>Por que do nome Yvy Chain?</Text>
+            <Text style={styles.textcoluna}>"Yvy", que deriva do guarani, língua indígena nativa da América do Sul, significa "terra" ou "natureza". Este termo carrega a importância e a beleza da natureza que nos cerca. "Chain", por sua vez, é uma alusão ao conceito de cadeia, um elo que une elementos de forma interdependente.  Nossa empresa acredita firmemente na integração harmoniosa entre natureza e tecnologia, reconhecendo que ambas são essenciais para um futuro sustentável.</Text>
+            </View>
+        </View>
+    </ScrollView>
     </View>
+    </ImageBackground>
   )
 }
