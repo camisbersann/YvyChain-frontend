6d68817bc9be3225434d074ebfe94fc3b0728560
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-24 09:23:17 -0300

    arrumando textos e imagens das divs

--- src/screens/Contribuition/index.jsx
+++ src/screens/Contribuition/index.jsx
@@ -8,10 +8,21 @@ export default function Contribuition() {
       <Title  style={styles.title} title={"SUA CONTRIBUIÇÃO"}/>
         <ScrollView>
             <Text style={styles.subtitulo}>Como a sua participação no projeto pode contribuir para o meio ambiente sustentavel e um desenvolvimento humano melhor?</Text>
+            <View>
             <View syle={styles.divpequena}>
-                <Image source={require('../../../assets/broto.jpg')} style={styles.image}/>
-                <Text style={styles.subtitulo}>Aqui você pode descrever como a sua contribuição pode ajudar o meio ambiente e o desenvolvimento humano.</Text>
+                <Image source={require('../../../assets/maos.jpg')} style={styles.image}/>
+                <Text style={styles.subtitulo}>Sua participação em nosso projeto promove sustentabilidade ambiental e desenvolvimento humano.</Text>
             </View>
+            <View>
+                <Image source={require('../../../assets/broto.jpg')} style={styles.image}/>
+                <Text style={styles.subtitulo}>Além disso, seu envolvimento contribui para uma educação mais inclusiva e capacita as pessoas.</Text>
+            </View>
+            <View>
+                <Image source={require('../../../assets/robo.jpg')} style={styles.image}/>
+                <Text style={styles.subtitulo}>Além disso, seu envolvimento contribui para uma educação mais inclusiva e capacita as pessoas.</Text>
+                </View>
+            </View>
+
         </ScrollView>
     </View>
   )
