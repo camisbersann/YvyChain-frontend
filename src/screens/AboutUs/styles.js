4d656925885a59d68b0300c4a097652b010e54d7
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-24 11:28:54 -0300

    finalizando colunas

--- src/screens/AboutUs/styles.js
+++ src/screens/AboutUs/styles.js
@@ -6,5 +6,31 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    coluna: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 20,
+    },
+    coluna1: {
+        width: '45%',
+    },
+    coluna2: {
+        width: '45%',
+        marginLeft: 10,
+    },
+    titulocoluna: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: "#EED2B8",
+        textAlign: 'left',
+        marginBottom: 10,
+        fontFamily: "Roboto",
+    },
+    textcoluna: {
+        fontSize: 14,
+        color: "#EED2B8",
+        marginTop: 10,
+        fontFamily: "Roboto",
+    },
 });
 export default styles;
