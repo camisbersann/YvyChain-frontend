3462d826fa8c158906bb81d0fad393974b5e0146
unknown <nicoly.ferreira@aluno.senai.br>
2024-04-24 08:22:41 -0300

    adicionando a pagina de contribuicao

---
+++ src/screens/Contribuition/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import styles from './styles';
+import Title from '../../components/Title';
+
+export default function Contribuition() {
+  return (
+    <View style={styles.container}>
+      <Title title={"Contribuition"}/>
+    </View>
+  )
+}
