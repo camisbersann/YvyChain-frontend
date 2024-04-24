d3ca092a9202ab63098131f83f91e638ef44a847
anaxcavalcante <ana.reis7@aluno.senai.br>
2024-04-24 10:48:20 -0300

    pagina home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -5,7 +5,31 @@ import Title from '../../components/Title';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Title title={"Home"}/>
+      <Title title={"Home"} />
+
+       <View style={styles.textStyle}>
+        <Text style={styles.text}>Bem vindo
+        </Text>
+        </View>
+
+      <View style={styles.divStyle}>
+        <Text>Nossa proposta</Text>
+        <Text>Conheça nossa missão e solução sustentável para o desenvolvimento humano.</Text>
+      </View>
+      
+      <View style={styles.divStyle}>
+        <Text>Sua contribuição</Text>
+        <Text>Entenda como sua colaboração nesse projeto pode promover o desenvolvimento.</Text>
+      </View>
+      
+      <View style={styles.divStyle}>
+        <Text>Conheça a Yvy</Text>
+        <Text>A nossa inteligência capaz de rastrear a pegada ecológica e a classificação de desenvolvimento da sua empresa.</Text>
+      </View>
+
+      <View style={styles.textStyle}>
+        <Text style={styles.text}>O projeto GreenChain é um sistema ambicioso que busca integrar sustentabilidade profundamente nos processos de produção industrial. Por meio de análises detalhadas e monitoramento contínuo, visa não apenas reduzir o impacto ambiental, mas também promover práticas de produção mais conscientes e responsáveis, alinhadas com os objetivos globais de sustentabilidade.</Text>
+      </View>
     </View>
-  )
+  );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,10 +1,27 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#3B5B30"
     },
+    divStyle: {
+        margin: 30,  
+        padding: 30, 
+        backgroundColor: "#EED2B8", 
+        borderRadius: 5, 
+        width: '80%', 
+        
+    },
+    text: {
+        color: "#EED2B8",
+        margin: 5,
+        fontFamily: 'sans-serif',
+    }
 });
+
 export default styles;
+
+
