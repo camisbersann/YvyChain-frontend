778d6b73769da1152e1d0f209056ffe5a4663a8f
unknown <nicoly.ferreira@aluno.senai.br>
2024-05-06 17:22:07 -0300

    pagina cadastro

--- src/screens/LoginScreen/index.jsx
+++ src/screens/LoginScreen/index.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Aboreto_400Regular } from '@expo-google-fonts/aboreto';
+import { ArimaMadurai_100Thin } from '@expo-google-fonts/arima-madurai'; // Corrigido para o nome correto, assumindo que você queria 'Arima Madurai'
+import { ReemKufiFun_400Regular } from '@expo-google-fonts/reem-kufi-fun';
+
 import axios from 'axios';
 import styles from './styles';
 
@@ -29,28 +34,42 @@ const LoginScreen = () => {
         setCnpj(''),
         setSenha('')
     ]
+    const [fontsLoaded] = useFonts({
+        Aboreto_400Regular,
+        ArimaMadurai_100Thin, // Corrigido para a importação correta
+        ReemKufiFun_400Regular,
+      });
+      if (!fontsLoaded) {
+          return <View><Text>Carregando fontes...</Text></View>;
+        }
 
     return (
         <View style={styles.container}>
+            <Text style={[styles.titulo, { fontFamily: 'Aboreto_400Regular' }]}>Login</Text>
+            <Text style={[styles.texto,  { fontFamily: 'ReemKufiFun_400Regular' }]}>Acesse essa área da sua empresa com as respectivas credenciais</Text>
+            <View style={styles.divinputs}>
             <TextInput
-            style={styles.textoinput}
+            style={[styles.textoinput,  { fontFamily: 'ReemKufiFun_400Regular' }]}
                 placeholder="Digite o CNPJ"
                 value={cnpj}
                 onChangeText={setCnpj}
                 keyboardType="numeric"
             />
             <TextInput
-            style={styles.textoinput}
+            style={[styles.textoinput,  { fontFamily: 'ReemKufiFun_400Regular' }]}
                 placeholder="Digite a senha (ID da empresa)"
                 value={senha}
                 onChangeText={setSenha}
                 secureTextEntry
             />
+            </View>
             <TouchableOpacity 
                 style={styles.botao}
                 onPress={handleLogin}>
-                <Text style={styles.textobotao}>Login</Text>
+                <Text style={[styles.textobotao, { fontFamily: 'Aboreto_400Regular' }]}>Entre</Text>
             </TouchableOpacity>
+            <Text style={[styles.texto,  { fontFamily: 'ReemKufiFun_400Regular' }]}>Não está no sistema?
+Entre em contato com o nosso suporte</Text>
         </View>
     );
 };
--- src/screens/LoginScreen/styles.js
+++ src/screens/LoginScreen/styles.js
@@ -3,8 +3,49 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex:1,
+        backgroundColor: "#44221F",
+    },
+    titulo: {
+        color: "#fff",
+        fontSize: 26,
+        textAlign: "center",
+        fontWeight: "bold",
+        marginTop: 50,
+    },
+    texto: {
+        color: "#fff",
+        fontSize: 14,
+        textAlign: "center",
+        margin: 20,
+    },
+    divinputs: {
+        margin: 20,
+        borderRadius: 10,
+        padding: 130,
+        backgroundColor: "#EED2B8",
         justifyContent: "center",
-        alignItems: "center",
+        width: 300,
+        alignSelf: "center",
+    },
+    botao: {
+        backgroundColor: "#C8E5BE",
+        borderRadius: 10,
+        padding: 10,
+        width: 150,
+        alignSelf: "center",
+        marginTop: -45,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    textobotao: {
+        textAlign: "center",
+        fontWeight: "bold",
     },
 });
 export default styles;
