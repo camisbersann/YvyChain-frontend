393a33d97aa19a8cb050f810c876e0a362975fd6
camisbersann <camila.bersan@aluno.senai.br>
2024-05-07 18:51:02 -0300

    feat: style Drawer.Screen LoginScreen

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -418,7 +418,32 @@ const DrawerRoutes = () => {
         },
       }}
       />
-      <Drawer.Screen name="LoginScreen" component={LoginScreen} />
+      <Drawer.Screen name="LoginScreen" component={LoginScreen} 
+       options={{
+        title: "Login",
+        headerTitle: "",
+        headerTitle: () => (
+          <View style={{ flexDirection: "row", alignItems: "right" }}>
+            <Image
+              source={require("../../assets/logo.png")}
+              style={{
+                width: 75,
+                height: 75,
+                marginLeft: 260,
+                marginBottom: 20,
+                marginTop: 18,
+              }}
+            />
+          </View>
+        ),
+        headerStyle: {
+          backgroundColor: "#3B5B30",
+        },
+        drawerStyle: {
+          backgroundColor: "#3B5B30",
+          textColor: "#fff",
+        },
+      }}/>
     </Drawer.Navigator>
   );
 };
