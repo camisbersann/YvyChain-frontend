3cf61a72c529ccc59eedd61ab6d5acd510c49487
camisbersann <camila.bersan@aluno.senai.br>
2024-05-07 18:39:00 -0300

    feat: style Drawer.Screen DetalhesEmpresa

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -87,6 +87,7 @@ const DrawerRoutes = () => {
           textColor: "#fff",
         },
       }}/>
+
       <Drawer.Screen name="CadastroEmpresa" component={CadastroEmpresa} 
        options={{
         title: "Cadastro Empresa",
@@ -113,6 +114,7 @@ const DrawerRoutes = () => {
           textColor: "#fff",
         },
       }}/>
+
       <Drawer.Screen name="VerEmpresas" component={VerEmpresas} 
        options={{
         title: "Ver Empresas",
@@ -139,11 +141,35 @@ const DrawerRoutes = () => {
           textColor: "#fff",
         },
       }}/>
+
       <Drawer.Screen name="DetalhesEmpresa" component={DetalhesEmpresa} 
       options={{
         drawerItemStyle: { height: 0 },  // Esconde o item no menu
-        unmountOnBlur: true  // Desmonta a tela ao perder o foco
-      }} 
+        unmountOnBlur: true,  // Desmonta a tela ao perder o foco
+          title: "Detalhes Empresa",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
       />
       <Drawer.Screen name="CadastroFornecedores" component={CadastroFornecedores} />
       <Drawer.Screen name="VerFornecedores" component={VerFornecedores} />
