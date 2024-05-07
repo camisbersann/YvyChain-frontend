df9d1eebe68e6d3dac64a70c10f212d7a901e590
camisbersann <camila.bersan@aluno.senai.br>
2024-05-07 18:29:02 -0300

    feat: style Drawer.Screen home

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -1,4 +1,5 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { View, Image } from "react-native";
 
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
@@ -31,7 +32,33 @@ const DrawerRoutes = () => {
         color: "white",
       },
     }} >
-      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Home" component={Home} 
+       options={{
+        title: "Home",
+        headerTitle: "",
+        headerTitle: () => (
+          <View style={{ flexDirection: "row", alignItems: "right" }}>
+            <Image
+              source={require("../../assets/logo.png")}
+              style={{
+                width: 75,
+                height: 75,
+                marginLeft: 260,
+                marginBottom: 20,
+                marginTop: 18,
+              }}
+            />
+          </View>
+        ),
+        headerStyle: {
+          backgroundColor: "#3B5B30",
+        },
+        drawerStyle: {
+          backgroundColor: "#3B5B30",
+          textColor: "#fff",
+        },
+      }}/>
+
       <Drawer.Screen name="Profile" component={Profile} />
       <Drawer.Screen name="Category" component={Category} />
       <Drawer.Screen name="Contribuition" component={Contribuition} />
